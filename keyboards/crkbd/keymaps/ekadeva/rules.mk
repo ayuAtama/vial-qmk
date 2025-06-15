186c0c332a56f7cab56bdaa73802c0e204914c38
Wahyu Pratama <ayuramatama@gmail.com>
2025-06-15 17:09:45 +0700

    Add and remove feature for the keyboard in ekadeva keymap

---
+++ keyboards/crkbd/keymaps/ekadeva/rules.mk
@@ -0,0 +1,15 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+TAP_DANCE_ENABLE 	= yes
+COMBO_ENABLE        = yes
+QMK_SETTINGS        = yes
+MOUSEKEY_ENABLE     = yes
+EXTRAKEY_ENABLE     = yes
+VIAL_INSECURE 		= yes
+OLED_ENABLE         = no
+OLED_DRIVER         = ssd1306
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+KEY_OVERRIDE_ENABLE = no
